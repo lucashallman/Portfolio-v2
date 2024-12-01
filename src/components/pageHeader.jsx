23883f64324952503997dc4980aebba9aa3598f0
lucashallman <lucas.hallmanc@gmail.com>
2024-12-01 13:11:02 -0600

    removed redundant code

--- src/components/pageHeader.jsx
+++ src/components/pageHeader.jsx
@@ -30,27 +30,6 @@ export default function pageHeader() {
                     </ul>
                 </nav>
 
-                {/* <table>
-                <tbody>
-                    <tr>
-                        <th><Link to='/'>Home</Link></th>
-                    </tr>
-                    <tr>
-                        <th><Link to='/About'>About</Link></th>
-                    </tr>
-                    <tr>
-                        <th><Link to='/Projects'>Portfolio</Link></th>
-                    </tr>
-                    <tr>
-                        <th><Link to='/Contact'>Contact</Link></th>
-                    </tr>
-                    <tr>
-                        <th><Link to='/Blog'>Blog</Link></th>
-                    </tr>
-                    <tr>
-                    </tr>
-                </tbody>
-            </table> */}
                 <div>
                     <h1>Lucas H.</h1>
                     {/* <p>The H stands for Hallman.</p> */}
